5d5d7d8208ac0c9539b2c14038100dfaec688cf3
oakinh <nikaohill@gmail.com>
2025-02-17 18:46:50 -0600

    Write null.c

---
+++ src/null.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+void dereferenceNull() {
+    int* ptr;
+
+    ptr = NULL;
+
+    int value = *ptr;
+}
+
+void forgetToFree() {
+    int* ptr = (int*)malloc(sizeof(int));
+}
+
+void data100() {
+    int* data = (int*)malloc(100 * sizeof(int));
+    data[100] = 0;
+}
+
+int main() {
+    //dereferenceNull();
+    //forgetToFree();
+    data100();
+    return 0;
+}
